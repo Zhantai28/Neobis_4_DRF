0317009b9a758975bb06f1e4bf1f1fa675e76044
Zhantai <Zhantai.Ismailov@gmail.com>
2021-06-01 16:46:01 +0600

    add serializers, view and urls, First working option

--- quickstart/views.py
+++ quickstart/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from .serializers import CourseSerializer
+from .models import Course
 
-# Create your views here.
+
+@csrf_exempt
+def course_list(request):
+    """
+    List all code snippets, or create a new snippet.
+    """
+
+    if request.method == 'GET':
+        courses = Course.objects.all()
+        serializer = CourseSerializer(courses, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = CourseSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+
+@csrf_exempt
+def course_detail(request, pk):
+    """
+    Retrieve, update or delete a code snippet.
+    """
+    try:
+        course = Course.objects.get(pk=pk)
+    except Course.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = CourseSerializer(course)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = CourseSerializer(course, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        course.delete()
+        return HttpResponse(status=204)
