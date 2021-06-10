a09748d1af5ebaecfeaa40e76840d209d828af67
Zhantai <Zhantai.Ismailov@gmail.com>
2021-06-10 17:54:05 +0600

    Use PostegreSQL instead of SQLlite, hide passwords and names

--- quickstart/admin.py
+++ quickstart/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(Category)
+admin.site.register(Course)
 admin.site.register(Branch)
 admin.site.register(Contact)
-admin.site.register(Course)
